5537a1d7fb5c88d21ed5e403a2268a9b67e84724
miracle101000 <okolomiracle76@outlook.com>
2026-03-29 17:33:26 +0100

    Now using commit on Android: 46910fcb27e865af6a302032a6b6b7a85a81581e

--- android/src/main/kotlin/com/revenuecat/purchases_ui_flutter/views/PaywallViewFactory.kt
+++ android/src/main/kotlin/com/revenuecat/purchases_ui_flutter/views/PaywallViewFactory.kt
@@ -7,14 +7,14 @@ import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-internal class PaywallFooterViewFactory(
+internal class PaywallViewFactory(
         private val messenger: BinaryMessenger,
         private val activity: () -> Activity?,
 ) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
         override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
                 @Suppress("UNCHECKED_CAST")
                 val creationParams = args as? Map<String?, Any?>? ?: emptyMap()
-                return PaywallFooterView(
+                return PaywallView(
                         context = activity() ?: context,
                         id = viewId,
                         messenger = messenger,
